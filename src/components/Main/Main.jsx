e8d98e940b1b1f2344905174d8ae982127abf141
Francsy <francsyludens@gmail.com>
2023-02-25 03:03:17 +0100

    add/ axios and get request to the nyt api

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,12 +5,23 @@ import ListNews from './ListNews'
 import Form from './Form'
 
 class Main extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      newArticles: []
+    }
+  }
+
+  addNew = article => this.setState({ news: [article, ...this.state.newArticles]})
+
+
   render() {
     return <main>
       <Routes>
       <Route path='/' element={<Home />}/>
-      <Route path='/list' element={<ListNews />}/>
-      <Route path='/form' element={<Form />}/>
+      <Route path='/list' element={<ListNews newArticles={this.state.newArticles} />} />
+      <Route path='/form' element={<Form addNew={this.addNew} />} />
       </Routes>
     </main>;
   }
