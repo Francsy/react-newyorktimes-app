c01455b7aae2a227b801943ffeac1b10350270cf
Francsy <francsyludens@gmail.com>
2023-02-25 13:15:13 +0100

    add/ form and list logic started

--- src/components/Main/ListNews/Card/Card.jsx
+++ src/components/Main/ListNews/Card/Card.jsx
@@ -2,7 +2,13 @@ import React, { Component } from "react";
 
 class Card extends Component {
   render() {
-    return <div>Card</div>;
+    return <article>
+      <img src={this.props.new.img} alt="" />
+      <h2>{this.props.new.title}</h2>
+      <h3>{this.props.new.date} - {this.props.new.section} - {this.props.new.authorLine}</h3>
+      <p>{this.props.new.abstract}</p>
+      <button onClick={this.props.delete}>Remove</button>
+    </article>;
   }
 }
 
